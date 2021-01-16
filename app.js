393d8295494870351db4ea9685cdfb0c30eeb8ef
DiazMauro <diaz.mauro.e@gmail.com>
2021-01-16 11:13:48 -0300

    me aburro

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.get ('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
 
-
-
 app.listen (process.env.PORT || 3000, () => 
     console.log('servidor corriendo!!!')
 );
